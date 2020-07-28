f33c37dec8171b58e0e72aeab4925e2db98e63eb
peder2911 <pglandsverk@gmail.com>
2020-07-28 16:03:26 +0200

    Moved migration to handler

--- main.go
+++ main.go
@@ -1,23 +1,11 @@
 
 package main
 import (
-   "github.com/jinzhu/gorm"
    "net/http"
    "log"
-   "fmt"
 )
 
 func main(){
-   db,err := gorm.Open("sqlite3","db.sqlite")
-   if err != nil {
-      panic("Could not open DB!" + fmt.Sprintf("%v",err))
-   }
-   defer db.Close()
-
-   db.AutoMigrate(&Variable{})
-   db.AutoMigrate(&Set{})
-   db.AutoMigrate(&Theme{})
-
    http.HandleFunc("/update/",update)
    http.HandleFunc("/",show)
    if err := http.ListenAndServe(":8080",nil); err != nil {
